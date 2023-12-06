2c7b12973acdb99e0881d4d242a1871785f5138c
Kloee Pratt-Castaneda <kloeepratt@gmail.com>
2023-12-06 12:37:46 -0600

    Initial Commit

---
+++ MuscleHouse/MuscleHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleHouseApp.swift
+//  MuscleHouse
+//
+//  Created by Kloee Pratt-Castaneda on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MuscleHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
